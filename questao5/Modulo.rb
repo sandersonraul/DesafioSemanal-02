28c1d5d8e2bfb7f62e8f0cd12ee1aaa5c1f04f86
Raul Oliveira <74179733+sandersonraul@users.noreply.github.com>
2021-06-15 21:23:05 -0300

    Concluo questao5

---
+++ questao5/Modulo.rb
@@ -0,0 +1,9 @@
+module AcoesDoCorpo
+  def movimentar_braco(idade)
+    if idade % 2 == 0
+      "Morreu dizendo que à teoria da relatividade é a maior descoberta do mundo"
+    else
+      "Morreu dizendo que à gravidade é a maior descoberta do mundo"
+    end 
+  end
+end 
